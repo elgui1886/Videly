2e1e72e2f4c52dc2d42c8cf1092c4fb33fec11f2
elgui1886 <elia.kobe@hotmail.it>
2016-12-29 18:02:09 +0100

    Inserito controller, model e view Movie. Inserito nuovo stile

---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    [RoutePrefix("Movies")]
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        [HttpGet]
+        [Route("Random")]
+        // GET: Movies/Random
+        public ActionResult Random()
+        {
+            //qui dovrei inserire una chiamata al mio db Locale
+            var movie = new Movie() {Name = "Shrek!"};
+            return View(movie);
+        }
+    }
+}
